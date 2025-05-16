b64def5d1c9b5a95a7605264b92a5851b150d7cc
Phuc20 <phucconghoang20@gmail.com>
2025-05-16 09:13:02 +0700

    capnhat2

--- darkmode.js
+++ darkmode.js
@@ -1,21 +1,19 @@
-let darkmode = localStorage.getItem('darkmode');
-const themeSwitch = document.getElementById('theme-switch');
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
 
 const enableDarkmode = () => {
-  document.body.classList.add('darkmode');
-  localStorage.setItem('darkmode', "active");
-};
-
-const disableDarkmode = () => {
-  document.body.classList.remove('darkmode');
-  localStorage.setItem('darkmode', null);
-};
-
-if (darkmode === "active") {
-  enableDarkmode();
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode', 'active')
 }
 
-themeSwitch.addEventListener('click', () => {
-  darkmode = localStorage.getItem('darkmode'); // cập nhật lại
-  darkmode !== "active" ? enableDarkmode() : disableDarkmode();
-});
+const disableDarkmode = () => {
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem('darkmode')
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
